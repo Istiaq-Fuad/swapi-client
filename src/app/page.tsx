8b7d19936f2dc57a280e9da5a48873a6c9befed3
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2025-02-15 17:00:16 +0600

    add logo and change favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import GlobalLoading from "@/components/GlobalLoading";
 import PeopleList from "@/components/PersonList";
 import SearchPeople from "@/components/SearchPeople";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home({}) {
   return (
     <div className="min-h-screen w-full">
+      <Link href="/">
+        <Image
+          className="absolute top-3 left-3"
+          src="/logo.svg"
+          alt="BabyYoda"
+          height={100}
+          width={100}
+        />
+      </Link>
+      
       <div className="bg-gray-100 w-full p-12">
         <SearchPeople />
       </div>
