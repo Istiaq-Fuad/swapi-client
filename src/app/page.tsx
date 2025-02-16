9df0d89bb2d942a0df8af0e35ecdf8104472b26a
Istiaqur Rahman Fuad <istiaqurrahmanfuad@gmail.com>
2025-02-16 11:29:07 +0600

    fix logo in small device

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home({}) {
         />
       </Link>
       
-      <div className="bg-gray-100 w-full p-12 mt-8 sm:mt-0">
+      <div className="bg-gray-100 w-full p-12 pt-20 sm:p-12">
         <SearchPeople />
       </div>
       <Suspense fallback={<GlobalLoading />}>
